3183af5ca9ae38302c07173d17f21e09d12975c3
Helenhyerim <hr4658@naver.com>
2021-11-20 12:33:02 +0900

    Config Class

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,25 @@
+package hello.core;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+// 구현 객체를 생성하고 연결함으로써 app 전반의 구성을 책임지는 Config
+public class AppConfig {
+
+    // 생성자 주입, DI
+    public MemberService memberService() {
+        return new MemberServiceImpl(new MemoryMemberRepository());
+    }
+
+    public OrderService orderService() {
+        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    }
+
+
+
+}
